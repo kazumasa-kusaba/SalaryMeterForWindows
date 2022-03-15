f4ba40e895399c9fbc2a0a5599c9840f5ef9d1d1
Kazumasa Kusaba <kazumasa@mail.kusaba.xyz>
2022-03-16 02:23:59 +0900

    Add State.cs

---
+++ SalaryMeterForWindows/State.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryMeterForWindows
+{
+    abstract class State
+    {
+        public void start()
+        {
+            // nothing to do
+        }
+        
+        public void pause()
+        {
+            // nothing to do
+        }
+
+        public void reset()
+        {
+            // nothing to do
+        }
+    }
+
+    class PauseState : State
+    {
+        private static PauseState pauseState = new PauseState();
+
+        private PauseState()
+        {
+            // singleton pattern
+            // nothing to do
+        }
+
+        public static PauseState getInstance()
+        {
+            return pauseState;
+        }
+    }
+
+    class RunState : State
+    {
+        private static RunState runState = new RunState();
+
+        private RunState()
+        {
+            // singleton pattern
+            // nothing to do
+        }
+
+        public static RunState getInstance()
+        {
+            return runState;
+        }
+    }
+
+    class ResetState : State
+    {
+        private static ResetState resetState = new ResetState();
+
+        private ResetState()
+        {
+            // singleton pattern
+            // nothing to do
+        }
+
+        public static ResetState getInstance()
+        {
+            return resetState;
+        }
+    }
+}
