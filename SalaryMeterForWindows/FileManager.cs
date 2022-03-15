772f1efbb05e86dbc9ded73b2798e9b9768bb141
Kazumasa Kusaba <kazumasa@mail.kusaba.xyz>
2022-03-16 02:32:49 +0900

    Add FileManager.cs

---
+++ SalaryMeterForWindows/FileManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryMeterForWindows
+{
+    class FileManager
+    {
+    }
+
+    class FileManagerForCoutUpSetting
+    {
+        private static FileManagerForCoutUpSetting fileManagerForCoutUpSetting = new FileManagerForCoutUpSetting();
+
+        private FileManagerForCoutUpSetting()
+        {
+            // singleton pattern
+            // nothing to do
+        }
+
+        public FileManagerForCoutUpSetting getInstance()
+        {
+            return fileManagerForCoutUpSetting;
+        }
+    }
+
+    class FileManagerForState
+    {
+        private static FileManagerForState fileManagerForState = new FileManagerForState();
+
+        private FileManagerForState()
+        {
+            // singleton pattern
+            // nothing to do
+        }
+
+        public FileManagerForState getInstance()
+        {
+            return fileManagerForState;
+        }
+    }
+}
