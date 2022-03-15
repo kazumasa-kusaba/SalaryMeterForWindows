afcf103d49299c3a23d4714983b44181f8ec5224
Kazumasa Kusaba <kazumasa@mail.kusaba.xyz>
2022-03-16 02:28:33 +0900

    Add Timer.cs

---
+++ SalaryMeterForWindows/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryMeterForWindows
+{
+    class Timer
+    {
+    }
+
+    interface CountUp
+    {
+    }
+
+    class CountUpAlgorithm1 : CountUp
+    {
+    }
+
+    class CountUpAlgorithm2 : CountUp
+    {
+    }
+}
