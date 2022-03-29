9a9a11e7dd68c788216dc729dda185ef37b982b5
Kazumasa Kusaba <kazumasa@mail.kusaba.xyz>
2022-03-30 01:57:08 +0900

    Fix SalaryMeter.cs

--- SalaryMeterForWindows/SalaryMeter.cs
+++ SalaryMeterForWindows/SalaryMeter.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SalaryMeterForWindows
 {
     class SalaryMeter
     {
+        private StateManager stateManager = StateManager.getInstance();
+
+        public void setAmountOfSalaryCallback(Action<uint> callback)
+        {
+            stateManager.setAmountOfSalaryCallback(callback);
+        }
+
+        public void setElapsedTimeSecCallback(Action<uint> callback)
+        {
+            stateManager.setElapsedTimeSecCallback(callback);
+        }
+
+        public void setSalaryPerHourCallback(Action<uint> callback)
+        {
+            stateManager.setSalaryPerHourCallback(callback);
+        }
+
+        public void setSalaryPerHour(uint salaryPerHour)
+        {
+            stateManager.setSalaryPerHour(salaryPerHour);
+        }
+
+        public void start()
+        {
+            stateManager.start();
+        }
+
+        public void pause()
+        {
+            stateManager.pause();
+        }
+
+        public void reset()
+        {
+            stateManager.reset();
+        }
+
+        public void setSalary()
+        {
+            stateManager.setSalary();
+        }
     }
 }
